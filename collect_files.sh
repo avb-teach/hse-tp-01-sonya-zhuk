e360795017fd02e27222f3c3798465264ce3e4f0
sonya-zhuk <sofiya.zhuk.06@mail.ru>
2025-04-29 23:43:21 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,73 +1,86 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 max_depth=""
-args=()
+pos=()
 
-while [ $# -gt 0 ]; do
-  case "$1" in
-    --max_depth)
-      if [[ "$2" =~ ^[0-9]+$ ]]; then
-        max_depth="$2"
-        shift 2
-      else
-        exit 1
-      fi
-      ;;
-    *)
-      args+=("$1")
-      shift
-      ;;
-  esac
-done
-
-if [ "${#args[@]}" -ne 2 ]; then
-  exit 1
-fi
-
-src_dir="${args[0]}"
-dst_dir="${args[1]}"
-
-if [ ! -d "$src_dir" ]; then
-  exit 1
-fi
-
-mkdir -p "$dst_dir"
-
-find "$src_dir" -type f | while IFS= read -r src_file; do
-  rel_path="${src_file#"$src_dir"/}"
-
-  if [ -n "$max_depth" ]; then
-    cur="$rel_path"
-    slash_count=0
-    while [ "${cur#*/}" != "$cur" ]; do
-      slash_count=$((slash_count + 1))
-      cur="${cur#*/}"
-    done
-    comp_count=$((slash_count + 1))
-    while [ "$comp_count" -gt "$max_depth" ]; do
-      rel_path="${rel_path#*/}"
-      comp_count=$((comp_count - 1))
-    done
-  fi
-
-  dest_path="$dst_dir/$rel_path"
-  dest_dir="${dest_path%/*}"
-  mkdir -p "$dest_dir"
-
-  base_name="${dest_path##*/}"
-  name_no_ext="${base_name%.*}"
-  extension="${base_name##*.}"
-  new_name="$base_name"
-  suffix=1
-
-  while [ -e "$dest_dir/$new_name" ]; do
-    if [ "$extension" != "$base_name" ]; then
-      new_name="${name_no_ext}_${suffix}.${extension}"
-    else
-      new_name="${name_no_ext}_${suffix}"
-    fi
-    suffix=$((suffix + 1))
+parse_args() {
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      --max_depth)
+        if [[ "$2" =~ ^[0-9]+$ ]]; then
+          max_depth="$2"
+          shift 2
+        else
+          exit 1
+        fi
+        ;;
+      *)
+        pos+=("$1")
+        shift
+        ;;
+    esac
   done
 
-  cp "$src_file" "$dest_dir/$new_name"
-done
+  if [ "${#pos[@]}" -ne 2 ]; then
+    exit 1
+  fi
+
+  input_dir="${pos[0]}"
+  output_dir="${pos[1]}"
+}
+
+compute_rel() {
+  local rel="$1"
+  if [ -n "$max_depth" ]; then
+    local tmp="$rel" slash_count=0 comps
+    while [ "${tmp#*/}" != "$tmp" ]; do
+      slash_count=$((slash_count + 1))
+      tmp="${tmp#*/}"
+    done
+    comps=$((slash_count + 1))
+    while [ "$comps" -gt "$max_depth" ]; do
+      rel="${rel#*/}"
+      comps=$((comps - 1))
+    done
+  fi
+  printf '%s' "$rel"
+}
+
+process_file() {
+  local file="$1"
+  local rel="${file#"$input_dir"/}"
+  rel=$(compute_rel "$rel")
+
+  local dest="$output_dir/$rel"
+  local dir="${dest%/*}"
+  local base="${dest##*/}"
+
+  mkdir -p "$dir"
+
+  local name="${base%.*}"
+  local ext="${base##*.}"
+  local new_base="$base"
+  local count=1
+
+  while [ -e "$dir/$new_base" ]; do
+    if [ "$ext" != "$base" ]; then
+      new_base="${name}_${count}.${ext}"
+    else
+      new_base="${name}_${count}"
+    fi
+    count=$((count + 1))
+  done
+
+  cp "$file" "$dir/$new_base"
+}
+
+main() {
+  parse_args "$@"
+  [ -d "$input_dir" ] || exit 1
+  mkdir -p "$output_dir"
+  find "$input_dir" -type f | while IFS= read -r file; do
+    process_file "$file"
+  done
+}
+
+main "$@"
