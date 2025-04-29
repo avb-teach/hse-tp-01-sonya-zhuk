4c1f00a83c5d5219b794131e43b6f534b3f9f4a6
sonya-zhuk <sofiya.zhuk.06@mail.ru>
2025-04-29 23:41:26 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,66 +1,73 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-MAX_DEPTH=0
-if [[ "${1:-}" == "--max_depth" ]]; then
-  shift
-  if (( $# < 3 )) || ! [[ "$1" =~ ^[0-9]+$ ]]; then
-    exit 1
-  fi
-  MAX_DEPTH=$1
-  shift
-fi
+max_depth=""
+args=()
 
-if (( $# != 2 )); then
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --max_depth)
+      if [[ "$2" =~ ^[0-9]+$ ]]; then
+        max_depth="$2"
+        shift 2
+      else
+        exit 1
+      fi
+      ;;
+    *)
+      args+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if [ "${#args[@]}" -ne 2 ]; then
   exit 1
 fi
 
-INPUT_DIR=$1
-OUTPUT_DIR=$2
+src_dir="${args[0]}"
+dst_dir="${args[1]}"
 
-if [[ ! -d "$INPUT_DIR" ]]; then
+if [ ! -d "$src_dir" ]; then
   exit 1
 fi
 
-mkdir -p "$OUTPUT_DIR"
-declare -A seen
+mkdir -p "$dst_dir"
 
-if (( MAX_DEPTH > 0 )); then
-  FIND_OPTS=( -maxdepth "$MAX_DEPTH" )
-else
-  FIND_OPTS=()
-fi
+find "$src_dir" -type f | while IFS= read -r src_file; do
+  rel_path="${src_file#"$src_dir"/}"
 
-find "$INPUT_DIR" "${FIND_OPTS[@]}" -type f -print0 |
-while IFS= read -r -d '' file; do
-  rel="${file#$INPUT_DIR/}"
-  dirpart=$(dirname "$rel")
-  base=$(basename "$rel")
-
-  if (( MAX_DEPTH > 0 )); then
-    dest_subdir="$OUTPUT_DIR/$dirpart"
-  else
-    dest_subdir="$OUTPUT_DIR"
-  fi
-  mkdir -p "$dest_subdir"
-
-  name="${base%.*}"
-  ext="${base##*.}"
-  if [[ "$name" == "$ext" ]]; then
-    ext=""
-  else
-    ext=".$ext"
+  if [ -n "$max_depth" ]; then
+    cur="$rel_path"
+    slash_count=0
+    while [ "${cur#*/}" != "$cur" ]; do
+      slash_count=$((slash_count + 1))
+      cur="${cur#*/}"
+    done
+    comp_count=$((slash_count + 1))
+    while [ "$comp_count" -gt "$max_depth" ]; do
+      rel_path="${rel_path#*/}"
+      comp_count=$((comp_count - 1))
+    done
   fi
 
-  newname="$name$ext"
-  if [[ -n "${seen[$newname]:-}" ]]; then
-    cnt=${seen[$newname]}
-    (( cnt++ ))
-    seen[$newname]=$cnt
-    newname="${name}_${cnt}${ext}"
-  else
-    seen[$newname]=1
-  fi
+  dest_path="$dst_dir/$rel_path"
+  dest_dir="${dest_path%/*}"
+  mkdir -p "$dest_dir"
 
-  cp -p -- "$file" "$dest_subdir/$newname"
+  base_name="${dest_path##*/}"
+  name_no_ext="${base_name%.*}"
+  extension="${base_name##*.}"
+  new_name="$base_name"
+  suffix=1
+
+  while [ -e "$dest_dir/$new_name" ]; do
+    if [ "$extension" != "$base_name" ]; then
+      new_name="${name_no_ext}_${suffix}.${extension}"
+    else
+      new_name="${name_no_ext}_${suffix}"
+    fi
+    suffix=$((suffix + 1))
+  done
+
+  cp "$src_file" "$dest_dir/$new_name"
 done
